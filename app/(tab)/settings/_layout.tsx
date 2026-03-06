7d8d7ece1189a81106960aa5fa775a607f67e8cf
Tess Unrau <70186751+zinc-saucier@users.noreply.github.com>
2026-03-06 13:06:38 -0700

    update

--- app/(tab)/settings/_layout.tsx
+++ app/(tab)/settings/_layout.tsx
@@ -4,7 +4,7 @@ export default function SettingsLayout() {
   return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Settings" }} />
-      <Stack.Screen name="profile" options={{ title: "Profile" }} />
+      
     </Stack>
   );
 }
